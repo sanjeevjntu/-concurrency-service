f3b2c21e6892821893a0b8cfdf9ae49fd9931df7
rajanalasanjeev <rajanala.sanjeev@gmail.com>
2020-02-03 00:01:42 -0500

    removed unused imports

--- src/main/java/com/sanjeev/rest/concurrencyservice/controller/UserController.java
+++ src/main/java/com/sanjeev/rest/concurrencyservice/controller/UserController.java
@@ -7,14 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
